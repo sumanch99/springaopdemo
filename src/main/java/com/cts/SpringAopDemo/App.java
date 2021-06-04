abe7f77b6871572e6e682980342c702435441d29
sumanch99 <sch.suman@gmail.com>
2021-06-04 08:57:44 +0530

    App.java changed

--- src/main/java/com/cts/SpringAopDemo/App.java
+++ src/main/java/com/cts/SpringAopDemo/App.java
@@ -6,6 +6,8 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 
+
+
 public class App {
 	public static void main(String[] args) {
 		
